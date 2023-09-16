f62568e4621377e32126411bbc0c1f7308a3c311
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-09-15 21:51:18 -0500

    error1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:candidateapp/listpage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,10 +6,12 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    title: "Candidate app",
-    home: MyApp(),
-  ));
+  runApp(MyApp()
+      // MaterialApp(
+      // title: "Candidate app",
+      // home: MyApp(),
+      // )
+      );
 }
 
 // ignore: must_be_immutable
@@ -18,73 +21,89 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                candidateReference.get().then((value) {
-                  QuerySnapshot candidateCollection = value;
-                  List<QueryDocumentSnapshot> docs = candidateCollection.docs;
-                  print("------------------");
-                  docs.forEach((element) {
-                    print(element.id);
-                    print(element.data());
-                  });
-                  print("************");
-                });
-              },
-              child: Text("Obtener data"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                candidateReference
-                    .doc("idCandidateX")
-                    .set(
-                      {
-                        "name": "ppk",
-                        'votes': 13,
-                        "direccion": "arequipa, peru"
-                      },
-                    )
-                    .then((value) {})
-                    .catchError((Error) {
-                      print(Error);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Home")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  candidateReference.get().then((value) {
+                    QuerySnapshot candidateCollection = value;
+                    List<QueryDocumentSnapshot> docs = candidateCollection.docs;
+                    print("------------------");
+                    docs.forEach((element) {
+                      print(element.id);
+                      print(element.data());
                     });
-              },
-              child: Text("Agregar"),
-            ),
-            ElevatedButton(
+                    print("************");
+                  });
+                },
+                child: Text("Obtener data"),
+              ),
+              ElevatedButton(
                 onPressed: () {
                   candidateReference
-                      .add({
-                        'name': 'Castillo',
-                        'votes': 16,
-                      })
+                      .doc("idCandidateX")
+                      .set(
+                        {
+                          "name": "ppk",
+                          'votes': 13,
+                          "direccion": "arequipa, peru"
+                        },
+                      )
                       .then((value) {})
                       .catchError((Error) {
                         print(Error);
                       });
-                  ;
                 },
-                child: Text("Insercion1")),
-            ElevatedButton(
-              onPressed: () {
-                candidateReference.doc("idCandidateX").update(
-                    {'name': 'Tomyssss', 'votes': 50, 'direccion': 'lima'});
-              },
-              child: Text("Actualizar"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                candidateReference.doc("idCandidateX").delete();
-              },
-              child: Text("Eliminar"),
-            ),
-          ],
+                child: Text("Agregar"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    candidateReference
+                        .add({
+                          'name': 'Castillo',
+                          'votes': 16,
+                        })
+                        .then((value) {})
+                        .catchError((Error) {
+                          print(Error);
+                        });
+                    ;
+                  },
+                  child: Text("Insercion1")),
+              ElevatedButton(
+                onPressed: () {
+                  candidateReference.doc("idCandidateX").update({
+                    'name': 'Tomyssss',
+                    'votes': 50,
+                    'direccion': 'lima',
+                  });
+                },
+                child: Text("Actualizar"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  candidateReference.doc("idCandidateX").delete();
+                },
+                child: Text("Eliminar"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ListPage(),
+                    ),
+                  );
+                },
+                child: Text("Ver lista"),
+              )
+            ],
+          ),
         ),
       ),
     );
