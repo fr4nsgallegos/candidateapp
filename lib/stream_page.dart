187c555db0230a589789783e5c14ef5c94208380
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-09-16 10:25:58 -0500

    streambuilder

--- lib/stream_page.dart
+++ lib/stream_page.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class StreamPage extends StatelessWidget {
+  CollectionReference candidateReference =
+      FirebaseFirestore.instance.collection("candidate");
+
   Stream<int> counter() async* {
     for (int i = 0; i < 10; i++) {
       yield i;
@@ -25,9 +29,42 @@ class StreamPage extends StatelessWidget {
     });
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("STREAM PAGE"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("STREAM PAGE"),
+        ),
+        body: StreamBuilder(
+            stream: candidateReference.snapshots(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                QuerySnapshot candidateCollection = snapshot.data;
+
+                return Center(
+                  child: Text(
+                    snapshot.data.toString(),
+                    style: TextStyle(fontSize: 50),
+                  ),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            })
+
+        //FUTURE BUILDER TRABAJA CON UN FUTURE
+        // FutureBuilder(
+        //   future: getNumber(),
+        //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+        //     if (snapshot.hasData) {
+        //       return Text(
+        //         snapshot.data.toString(),
+        //         style: TextStyle(fontSize: 50),
+        //       );
+        //     }
+        //     return Center(
+        //       child: CircularProgressIndicator(),
+        //     );
+        //   },
+        // ),
+        );
   }
 }
