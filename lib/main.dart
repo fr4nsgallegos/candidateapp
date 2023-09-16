494fd72950849e878dd069f085ec3ac80648f08b
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-09-15 21:19:43 -0500

    avance2

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,56 @@ void main() async {
   ));
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  CollectionReference candidateReference =
+      FirebaseFirestore.instance.collection('candidate');
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference candidateReference =
-        FirebaseFirestore.instance.collection('candidates');
-    candidateReference.get().then((value) {
-      QuerySnapshot candidateCollection = value;
-      List<QueryDocumentSnapshot> docs = candidateCollection.docs;
-      print("------------------");
-      // print(docs.first.data());
-      docs.forEach((element) {
-        print(element.id);
-        print(element.data());
-      });
-    });
-    // print("****************");
-    // print(candidateReference);
-
     return Scaffold(
       appBar: AppBar(title: Text("Home")),
-      body: Column(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                candidateReference.get().then((value) {
+                  QuerySnapshot candidateCollection = value;
+                  List<QueryDocumentSnapshot> docs = candidateCollection.docs;
+                  print("------------------");
+                  docs.forEach((element) {
+                    print(element.id);
+                    print(element.data());
+                  });
+                  print("************");
+                });
+              },
+              child: Text("Obtener data"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                candidateReference
+                    .doc("idCandidateY")
+                    .set(
+                      {
+                        "name": "ppk",
+                        'votes': 13,
+                        "direccion": "arequipa, peru"
+                      },
+                    )
+                    .then((value) {})
+                    .catchError((Error) {
+                      print(Error);
+                    });
+              },
+              child: Text("Agregar"),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Insercion1"))
+          ],
+        ),
+      ),
     );
   }
 }
